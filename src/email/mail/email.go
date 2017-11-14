df2cd037286f988d0aeb6397ffc72de9518003ab
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-11-14 17:38:27 +0700

    add send email

--- src/email/mail/email.go
+++ src/email/mail/email.go
@@ -1,6 +1,17 @@
 package mail
 
-// Message represents an email.
+import (
+	"fmt"
+	"log"
+
+	"gopkg.in/mailgun/mailgun-go.v1"
+	sendgrid "gopkg.in/sendgrid/sendgrid-go.v2"
+)
+
+const mg = mailgun.NewMailgun(yourdomain, ApiKey, publicApiKey)
+const sg = sendgrid.NewSendGridClient("sendgrid_user", "sendgrid_key")
+
+// Email represents an email.
 type Email struct {
 	To      string
 	From    string
@@ -15,7 +26,7 @@ func New() *Email {
 	return &Email{}
 }
 
-// SetContent method
+// Set method
 func (e *Email) Set(To string, From string, Subject string, Body string, Cc string, Bcc string) *Email {
 	return &Email{
 		To:      To,
@@ -24,5 +35,45 @@ func (e *Email) Set(To string, From string, Subject string, Body string, Cc stri
 		Body:    Body,
 		Cc:      Cc,
 		Bcc:     Bcc,
+	}
+}
+
+func (e *Email) Send() error {
+
+	err := mailGun(e)
+	if err != nil {
+		log.Fatal(err)
+		err = sendGrid(e)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func mailGun(e *Email) error {
+	message := mailgun.NewMessage(
+		e.From,
+		e.Subject,
+		e.Body,
+		e.To)
+	resp, id, err := mg.Send(message)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	fmt.Printf("ID: %s Resp: %s\n", id, resp)
+}
+
+func sendGrid(e *Email) error {
+
+	message := sendgrid.NewMail()
+	message.AddTo(e.To)
+	message.SetSubject(e.Subject)
+	message.SetHTML(e.Body)
+	message.SetFrom(e.From)
+	_, err := sg.Send(message)
+	if err != nil {
+		log.Fatal(err)
+		return err
 	}
 }
