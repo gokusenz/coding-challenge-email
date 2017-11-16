7a813aef3470110cb18ecce0f683c492e1c5266c
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-11-16 20:28:53 +0700

    refactor (add logs)

--- src/email/main.go
+++ src/email/main.go
@@ -10,15 +10,41 @@ import (
 	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/justinas/alice"
 )
 
-func handler(rw http.ResponseWriter, r *http.Request) {
+func recoverHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic: %+v", err)
+				http.Error(w, http.StatusText(500), 500)
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func loggingHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		t2 := time.Now()
+		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func indexHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(rw, "Welcome!")
 }
 
-func handlerEmail(rw http.ResponseWriter, r *http.Request) {
+func emailHandler(rw http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	t1 := time.Now()
 	decoder := json.NewDecoder(r.Body)
 	var e *mail.Email
 	err := decoder.Decode(&e)
@@ -36,8 +62,6 @@ func handlerEmail(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(resp)
 		rw.Write([]byte("Success"))
 	}
-	t2 := time.Now()
-	log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
 }
 
 func main() {
@@ -53,8 +77,9 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/email", handlerEmail)
+	commonHandlers := alice.New(loggingHandler, recoverHandler)
+	http.Handle("/", commonHandlers.ThenFunc(indexHandler))
+	http.Handle("/email", commonHandlers.ThenFunc(emailHandler))
 	http.ListenAndServe(":8080", nil)
 
 	// e := &mail.Email{
