593907308aca7e2f6e2bc7e03c75d8d8ef674740
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-11-20 14:01:59 +0700

    add EmailForm

---
+++ frontend/src/components/EmailForm.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import InputText from './InputText'
+import TextArea from './TextArea'
+
+const EmailForm = ({ handleSubmit, handleChange, to, from, subject, body, cc, bcc }) => (
+  <form className="form-horizontal col-md-9 col-md-offset-1 col-xs-12" onSubmit={handleSubmit}>
+    <div className="form-group">
+      <label htmlFor="daily_date" className="col-md-3 col-sm-2 control-label">To</label>
+      <div className="col-md-8 col-sm-10" >
+        <InputText name="to" type="text" defaultValue={to} handleChange={handleChange} />
+      </div>
+    </div>
+    <div className="form-group">
+      <label htmlFor="cc" className="col-md-3 col-sm-2 control-label">Cc</label>
+      <div className="col-md-8 col-sm-10" >
+        <InputText name="cc" type="text" defaultValue={cc} handleChange={handleChange} />
+      </div>
+    </div>
+    <div className="form-group">
+      <label htmlFor="bcc" className="col-md-3 col-sm-2 control-label">Bcc</label>
+      <div className="col-md-8 col-sm-10" >
+        <InputText name="bcc" type="text" defaultValue={bcc} handleChange={handleChange} />
+      </div>
+    </div>
+    <div className="form-group">
+      <label htmlFor="from" className="col-md-3 col-sm-2 control-label">From</label>
+      <div className="col-md-8 col-sm-10" >
+        <InputText name="from" type="text" handleChange={handleChange} />
+      </div>
+    </div>
+    <div className="form-group">
+      <label htmlFor="subject" className="col-md-3 col-sm-2 control-label">Subject</label>
+      <div className="col-md-8 col-sm-10" >
+        <InputText name="subject" type="text" handleChange={handleChange} />
+      </div>
+    </div>
+    <div className="form-group">
+      <label htmlFor="body" className="col-md-3 col-sm-2 control-label">Body</label>
+      <div className="col-md-8 col-sm-10" >
+        <TextArea name="body" row="5" handleChange={handleChange} />
+      </div>
+    </div>
+    <div className="form-group">
+      <div className="col-sm-offset-2 col-sm-10">
+        <button type="submit" className="btn btn-success">Submit</button>
+      </div>
+    </div>
+  </form>
+)
+
+export default EmailForm
---
+++ frontend/src/containers/EmailForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import EmailFormComponent from '../components/EmailForm';
+
+export class EmailForm extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      to: '',
+      from: '',
+      subject: '',
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target.to.value);
+    // if (result) {
+    //   alert('บันทึกข้อมูลเรียบร้อย')
+    //   e.target.to.value = ''
+    // } else {
+    //   alert('ส่งข้อมูลไม่สำเร็จ กรุณาลองใหม่อีกครั้ง')
+    // }
+  }
+
+  handleChange = (event, fieldName) => {
+    let state = {}
+    state[fieldName] = event.target.value
+    console.log(event.target.value)
+    this.setState(state)
+  }
+
+  render() {
+    const { to, from, subject, body, cc, bcc } = this.props;
+    return (
+      <EmailFormComponent handleSubmit={this.handleSubmit} handleChange={this.handleChange} to={to} from={from} subject={subject} body={body} cc={cc} bcc={bcc} />
+    );
+  }
+}
+
+export default EmailForm
